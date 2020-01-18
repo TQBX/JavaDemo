552d94c76f274e868b1c3e09cf56b48af1f0252f
TQBX <hyh1332790762@163.com>
2020-01-18 22:09:04 +0800

    "IO update"

---
+++ module01/src/com/my/IO流/FileReaderDemo1.java
@@ -0,0 +1,26 @@
+package com.my.IO流;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * @auther Summerday
+ */
+public class FileReaderDemo1 {
+    public static void main(String[] args) throws IOException {
+        FileReader reader = new FileReader("D:\\b.txt");
+
+        //定义数组作为缓冲区
+        char[] cs = new char[5];
+        //将数据读取到缓冲区中
+//        reader.read(cs);
+//        System.out.println(new java.lang.String(cs));
+
+
+
+        int len;
+        while((len =reader.read(cs))!=-1){
+            System.out.println(new String(cs,0,len));
+        }
+        reader.close();
+    }
+}
