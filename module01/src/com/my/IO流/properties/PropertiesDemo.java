547766fb0bce4d578564790c2cae3832af587c83
TQBX <hyh1332790762@163.com>
2020-02-07 21:42:40 +0800

    "Thread"

--- module01/src/com/my/IO流/properties/PropertiesDemo.java
+++ module01/src/com/my/IO流/properties/PropertiesDemo.java
@@ -9,18 +9,20 @@ import java.util.Properties;
  */
 public class PropertiesDemo {
     public static void main(String[] args) throws IOException {
+        //创建Properties对象
         Properties prop = new Properties();
 
         //prop是映射 存储的是键值对  都是String类型
         //添加元素
-        prop.setProperty("姓名","杰克");
+        prop.setProperty("姓名","天乔巴夏丶");
 
         //用于序列化的方法
         //文件后缀名必须是 .properties
 
         //comments  注释
-        FileOutputStream out = new FileOutputStream("person.properties");
-        prop.store(out, "this is a person");
+        FileOutputStream out = new FileOutputStream(
+                "E:\\java_learning\\project01\\module01\\src\\com\\my\\IO流\\properties\\person.properties");
+        prop.store(out, "this is me");
 
         out.close();
 
--- module01/src/com/my/concurrency/ThreadDemo.java
+++ module01/src/com/my/concurrency/ThreadDemo.java
@@ -7,56 +7,84 @@ import java.util.concurrent.*;
  * @auther Summerday
  */
 public class ThreadDemo extends Thread {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-//        TDemo t1 = new TDemo("t1");
+    public static void main(String[] args){
+//        TDemo t1 = new TDemo();
 //        //启动线程
 //        //start会给线程做很多配置,调用start和调用run不一样的
 //        //配置完成之后会自动调用run方法执行指定的任务
-//        t1.start();
+//        t1.run();
+
 //        TDemo t2 = new TDemo("t2");
-//        t2.start();
+//       t2.start();
+//        System.out.print("main");
 
-//        RDemo r = new RDemo("r");
-//        Thread t = new Thread(r);
-//        t.start();
 
-        CDemo c = new CDemo();
-        //执行器服务 执行器助手
-        ExecutorService es = Executors.newCachedThreadPool();
-        Future<String> f = es.submit(c);
-        System.out.println(f.get());
-        es.shutdown();
+
+
+
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                for (int i = 0; i < 5; i++) {
+//                    System.out.print(Thread.currentThread().getName()+i);
+//                }
+//            }
+//        }).start();
+
+//        CDemo c = new CDemo();
+//        //执行器服务 执行器助手
+//        ExecutorService es = Executors.newCachedThreadPool();
+//        Future<String> f = es.submit(c);
+//        System.out.println(f.get());
+//        es.shutdown();
+
+        FutureTask<Integer> task = new FutureTask<>(new Callable<Integer>() {
+
+            @Override
+            public Integer call() throws Exception {
+                int i = 0;
+                while(i<5){
+                    System.out.println(Thread.currentThread().getName());
+                    i++;
+                }
+                return i;
+            }
+        });
+        for(int i = 0;i<5;i++){
+            System.out.println(Thread.currentThread().getName());
+            if(i == 4){
+                new Thread(task,"有返回值的线程").start();
+            }
+        }
+        try {
+            System.out.println(task.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 }
 
 //创建一个线程类
 class TDemo extends Thread{
-    private String name;
-    public TDemo(String name){
-        this.name = name;
-    }
     //线程要执行的任务在run方法中
     @Override
     public void run(){
-        for (int i = 0; i < 10; i++) {
-            System.out.println(name+":"+i);
+        for (int i = 0; i < 5; i++) {
+            System.out.print(this.getName()+i);
         }
     }
 }
-//实现Runnable接口
-class RDemo implements Runnable{
-    private String name;
-    public RDemo(String name){
-        this.name = name;
-    }
-
-    @Override
-    public void run() {
-        for (int i = 0; i < 10; i++) {
-            System.out.println(name+":"+i);
-        }
-    }
-}
+////实现Runnable接口
+//class RDemo implements Runnable{
+//    @Override
+//    public void run() {
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println(i);
+//        }
+//    }
+//}
 
 //实现Callable<T>接口
 class CDemo implements Callable<String>{
